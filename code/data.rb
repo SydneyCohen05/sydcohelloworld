86be146037bf2f9e596d1f8a426fae0a04ad66ed
SydneyCohen05 <129341620+SydneyCohen05@users.noreply.github.com>
2023-03-29 20:50:22 -0500

    Initial commit

---
+++ code/data.rb
@@ -0,0 +1,17 @@
+# To run this code, be sure your current working directory
+# is the same as where this file is located and then run:
+# ruby data.rb
+
+# In Ruby, there are different *types* of data:
+
+# - Numbers
+
+# - Perform simple math with numbers
+
+# - Strings
+
+# - Combine strings together
+
+# - Variables
+
+# - Combine strings and variables
